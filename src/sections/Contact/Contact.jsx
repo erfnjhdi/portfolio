0ac3f09cf11ee767eba24ca6b81d97b467bb7975
Erfan <erfanjahedi81@gmail.com>
2024-11-16 16:02:04 -0500

    contact fix

--- src/sections/Contact/Contact.jsx
+++ src/sections/Contact/Contact.jsx
@@ -12,7 +12,6 @@ function Contact() {
       <h1 className="sectionTitle">Contact</h1>
       <h4>I'd love to hear from you! Here are some ways to reach out:</h4>
       <div className={styles.list}>
-      <p><b>Phone:</b> 438.928.7493</p>
         <p><b>Email: </b><a href="mailto:erfanjahedi81@gmail.com" target='_blank' rel="noopener noreferrer">erfanjahedi81@gmail.com</a></p>
         <p><b>LinkedIn:</b>
           <a href="https://www.linkedin.com/in/erfan-jahedi-b46568295/" target="_blank">
